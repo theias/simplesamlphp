3dba553197c1a78dd31be368c412899af2f7690c
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2024-09-06 13:51:19 -0400

    Actions/rpmbuild: actually use git again

--- extra/ias.rpmbuild.sh
+++ extra/ias.rpmbuild.sh
@@ -6,7 +6,6 @@ vers="${1:-$(git tag | tail -n1)}"
 mkdir -p ./rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 mkdir -p "./simplesamlphp-${vers}"
 
-# git archive --format=tar "${vers}" | tar --directory="./archive/simplesamlphp-${vers}" -xf -
 git clone . "./simplesamlphp-${vers}"
 tar -cf ./rpmbuild/SOURCES/"simplesamlphp-${vers}.tar.gz" "./simplesamlphp-${vers}"
 
