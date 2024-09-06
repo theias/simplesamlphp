53acd39d397e9c0ef66f260b14057e88967a0e60
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2024-09-06 13:48:45 -0400

    Actions/rpmbuild: take tag name from the workflow instead of using git

--- extra/ias.rpmbuild.sh
+++ extra/ias.rpmbuild.sh
@@ -2,7 +2,7 @@
 set -eu
 set -o pipefail
 
-vers="$(git tag | tail -n1)"
+vers="${1:-$(git tag | tail -n1)}"
 git checkout "${vers}"
 mkdir -p ./rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 mkdir -p "./simplesamlphp-${vers}"
