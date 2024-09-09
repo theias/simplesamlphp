9f951496b0479c185ac34b6be7174d903664d10a
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2024-09-09 10:40:35 -0400

    dev workflow

--- extra/ias.rpmbuild.sh
+++ extra/ias.rpmbuild.sh
@@ -10,7 +10,7 @@ cp -a . "/tmp/simplesamlphp-${vers}/"
 tar -cf ./rpmbuild/SOURCES/"simplesamlphp-${vers}.tar.gz" "/tmp/simplesamlphp-${vers}"
 
 
-cp extra/simplesamlphp.spec rpmbuild/SPECS/simplesamlphp.spec
+cp extra/ias.simplesamlphp.spec rpmbuild/SPECS/simplesamlphp.spec
 sed -i "s/^%define version  .*\$/%define version   ${vers}/" rpmbuild/SPECS/simplesamlphp.spec
 ls -R rpmbuild
 fgrep '%define version' rpmbuild/SPECS/simplesamlphp.spec
